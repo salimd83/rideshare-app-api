ede7f5c943f5e694449138a5b5c4b1944c0471c6
Salim Dirani <salimdirani@gmail.com>
2021-04-01 23:18:53 +0300

    validating user endpoint data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 
@@ -12,56 +12,85 @@ app.use(cors());
 app.use(express.json());
 
 const client = new MongoClient(dbURL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
 let db;
 
 async function runConnect() {
-    try {
-        await client.connect();
-        db = client.db(dbName);
-        const users = db.collection('users');
-        users.createIndex({email: 1}, {unique: 1});
+  try {
+    await client.connect();
+    db = client.db(dbName);
+    const users = db.collection("users");
+    users.createIndex({ email: 1 }, { unique: 1 });
 
-        console.log('The database is connected');
-    } catch (error) {
-        console.error("Couldn't connect to database.");
-        console.log(error);
-    }
+    console.log("The database is connected");
+  } catch (error) {
+    console.error("Couldn't connect to database.");
+    console.log(error);
+  }
 }
 
 runConnect();
 
 const handleNewUser = (user) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const users = db.collection('users');
-            const results = await users.insertOne(user);
-            resolve(results.insertedId);
-        } catch (error) {
-            reject(error);
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      const users = db.collection("users");
+      const results = await users.insertOne(user);
+      resolve(results.insertedId);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
 
-app.get('/', (req, res) => {
-    res.send('Hello!');
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
-app.post('/users', async (req, res) => {
-    const user = req.body;
-    try {
-        const results = await handleNewUser(user);
-        res.send(results)
-    } catch (error) {
-        if(error.message.startsWith('E11000 duplicate'))
-            res.status(500).send('duplicate email.');
-        else res.sendStatus(500);
-    }
+function isEmail(email) {
+  const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return email.match(mailformat);
+}
 
+function isPassword(pass) {
+  const passformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
+  return pass.match(passformat);
+}
+
+function validateUser(user) {
+  const errors = [];
+  if (!user.firstName) errors.push("Firstname is required");
+  if (!user.lastName) errors.push("Lastname is required");
+  if (!user.email) {
+    errors.push("Email is required");
+  } else {
+    if (!isEmail(user.email)) errors.push("Wrong email format.");
+  }
+  if (!user.password) {
+    errors.push("Password is required");
+  } else {
+    if (!isPassword(user.password))
+      errors.push(
+        "Wrong password format it must be between 8 to 15 characters which contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character."
+      );
+  }
+  return errors;
+}
+
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  try {
+      if(validateUser(user).length > 0) throw new Error("validation error.");
+      const results = await handleNewUser(user);
+      res.send(results);
+  } catch (error) {
+    if (error.message.startsWith("E11000 duplicate"))
+      res.status(500).send("duplicate email.");
+    else res.status(500).send(error.message);
+  }
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
-
