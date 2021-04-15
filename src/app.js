3eac932f7d685818875aad73e3d07ecc24415a76
Salim Dirani <salimdirani@gmail.com>
2021-04-15 22:54:07 +0300

    login with JWT

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
 const Joi = require("joi");
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(cors());
@@ -39,7 +40,7 @@ const requestHandler = (db) => async (req, res) => {
     value.password = await bcrypt.hash(value.password, 8);
 
     const results = await createUser(
-      { ...value, isEmailVerified: false, created_at: new Date() },
+      { ...value, isEmailVerified: false, createdAt: new Date() },
       db
     );
     res.status(201).send(results);
@@ -50,8 +51,40 @@ const requestHandler = (db) => async (req, res) => {
   }
 };
 
+const getUserByEmail = async (email, db) => {
+  const users = db.collection('users');
+  return await users.findOne({email});
+}
+
+const loginRequestHandler = (db) => async (req, res) => {
+  const {email, password} = req.body;
+  const message = "Wrong email/password combination";
+
+  if(!email || !password) return res.status(400).send({message: 'validation error'});
+  const user = await getUserByEmail(email, db);
+  if(!user) return res.status(401).send({message});
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) return res.status(401).send({message});
+
+  const token = jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET, {
+    expiresIn: req.body.remember ? '7d' : '1h'
+  });
+
+  res.send({
+    user: {
+      name: user.name,
+      email: user.email,
+      isEmailVerified: user.isEmailVerified,
+      createdAt: user.createdAt
+    },
+    token
+  })
+}
+
 app.setupRoutes = (db) => {
   app.post("/users", requestHandler(db));
+  app.post("/users/login", loginRequestHandler(db));
 };
 
 module.exports = app;
