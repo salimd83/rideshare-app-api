66ca20345fdcc37232007243f98b49f950f5ccc1
Salim Dirani <salimdirani@gmail.com>
2021-04-15 21:18:09 +0300

    login

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
+const Joi = require("joi");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(cors());
@@ -18,46 +20,75 @@ const createUser = (user, db) => {
   });
 };
 
-function isEmail(email) {
-  return email.match(
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-  );
-}
-
-function isPassword(pass) {
-  return pass.match(
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/
-  );
-}
-
-function validateUser(user) {
-  const errors = '';
-  if (!user.name) errors.concat(", ", "Firstname is required");
-  if (!user.email) errors.concat(", ", "Email is required");
-  else if (!isEmail(user.email)) errors.concat(", ", "Wrong email format");
-  if (!user.password) errors.concat(", ", "Password is required");
-  else if (!isPassword(user.password)) errors.concat(", ", "Wrong password format");
-  return errors.substring(1);
-}
+const schema = Joi.object({
+  name: Joi.string().min(4).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string()
+    .required()
+    .pattern(
+      new RegExp(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/
+      )
+    ),
+});
 
 const requestHandler = (db) => async (req, res) => {
-  const user = req.body;
+  const { name, email, password } = req.body;
   try {
-    const errors = validateUser(user);
-    if (errors.length > 0)
-      res.status(400).send({ message: 'validation error', details: errors });
-    user.password = await bcrypt.hash(user.password, 8);
-    const results = await createUser(user, db);
+    const { value, error } = schema.validate({ name, email, password });
+
+    if (error) return res.status(400).send({ error });
+
+    value.password = await bcrypt.hash(value.password, 8);
+
+    const results = await createUser(
+      { ...value, isEmailVerified: false, createdAt: new Date() },
+      db
+    );
     res.status(201).send(results);
   } catch (error) {
     if (error.code === 11000)
       res.status(400).send({ message: "duplicate email." });
     else res.status(500).send(error.message);
   }
-}
+};
+
+const getUserByEmail = async (email, db) => {
+  const userCollection = db.collection("users");
+  return await userCollection.findOne({ email });
+};
+
+const loginRequestHandler = (db) => async (req, res) => {
+  const { email, password } = req.body;
+  const message = "Wrong email/password combination";
+
+  if (!email || !password)
+    return res.status("400").send({ message: "validation error" });
+
+  const user = await getUserByEmail(email, db);
+  if (!user) return res.status("401").send({ message });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status("401").send({ message });
+
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, {
+    expiresIn: req.body.remember ? "7d" : "1h",
+  });
+
+  res.send({
+    user: {
+      name: user.name,
+      email: user.email,
+      isEmailVerified: user.isEmailVerified,
+      createdAt: user.createdAt,
+    },
+    token,
+  });
+};
 
 app.setupRoutes = (db) => {
   app.post("/users", requestHandler(db));
+  app.post("/users/login", loginRequestHandler(db));
 };
 
 module.exports = app;
